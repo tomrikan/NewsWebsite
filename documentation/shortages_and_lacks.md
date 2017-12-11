a7f4e10f58e3304d3516320bcfbcfb2896e5b5e1
Tomi Rikander <Tomi@TomiRiknderiMac.home>
2017-12-11 18:05:34 +0200

    testfile, updates

--- documentation/shortages_and_lacks.md
+++ documentation/shortages_and_lacks.md
@@ -1,8 +1,9 @@
 # Lacks and needs for improvement
 
 * Log-in for staff
-* Show management panel only when logged in
-* Style
-* Photo showing doesn't work
-* Most viewed should be for last weak only
+* Show management panel _only_ when logged in
+* No css style
+* Showing photos of news-items doesn't work
+* Most viewed should be for last week only
 * Show top 5 most viewed on every news-item pages
+* No tests yet
---
+++ src/test/java/wad/controller/NewsWebsiteTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad.controller;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ *
+ * @author Tomi
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsWebsiteTest {
+    
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+    
+   @Test
+    public void statusOk() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+    
+    
+
+}
